c5a7c5cc89194939bbc8418d619131732fc42c24
jeffersondenilson <jefersond121212@gmail.com>
2021-01-25 19:56:06 -0300

    adicionados componentes

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,43 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json'
+import Head from 'next/head'
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      <Head>
+        <title>Quiz de Yu-Gi-Oh!</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Quiz de Yu-Gi-Oh!" key="title" />
+      </Head>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
