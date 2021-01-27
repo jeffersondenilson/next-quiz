4e576abfd341fe06df218488a02026e33525315c
jeffersondenilson <jefersond121212@gmail.com>
2021-01-27 17:14:44 -0300

    adicionada opacidade

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -12,12 +12,22 @@ const Button = styled.button`
   font-weight: bold;
   cursor: ${({ disabled }) => (disabled ? 'arrow' : 'pointer')};
   outline: none;
-  box-shadow: ${({ disabled }) => (disabled ? '0 0' : '4px 4px 0 1px #ba124a')};
+  box-shadow: 4px 4px 0 1px #888;
+  &:hover{
+    opacity: 0.9;
+  }
   &:active {
-    background-color: #e8175d;
-    box-shadow: 2px 2px 0 1px #8b0e38;
+    // background-color: #e8175d;
+    background-color: ${({ theme }) => theme.colors.secondary};
+    opacity: 0.7;
+    box-shadow: 2px 2px 0 1px #666;
     transform: translateY(4px);
   }
 `;
 
 export default Button;
+
+/*
+box-shadow: ${({ disabled }) => (disabled ? '0 0' : '4px 4px 0 1px #ba124a')};
+box-shadow: 2px 2px 0 1px #8b0e38;
+*/
