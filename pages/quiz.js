8560a575a939e15e2439dba53e6842b2962d1730
jeffersondenilson <jefersond121212@gmail.com>
2021-01-27 06:35:22 -0300

    background, widget e nqme atraves do router

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import db from '../db.json';
 import QuizBackground from '../src/components/QuizBackground';
-import GitHubCorner from '../src/components/GitHubCorner';
+import QuizContainer from '../src/components/QuizContainer';
+import Widget from '../src/components/Widget';
 
 export default function QuizPage() {
+  const router = useRouter();
+  const { name } = router.query;
+
   return (
-    <div>quiz</div>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{name ? `Olá, ${name}` : ''}</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+    </QuizBackground> 
   );
 }
