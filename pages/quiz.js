72ab793fc4137571a667952bd64b7a3d5c85c0bb
jeffersondenilson <jefersond121212@gmail.com>
2021-01-27 20:06:44 -0300

    quiz e loading

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,41 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import db from '../db.json';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizContainer from '../src/components/QuizContainer';
 import QuizLogo from '../src/components/QuizLogo';
 import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Form from '../src/components/Form';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
+import LoadingWidget from '../src/components/LoadingWidget';
+import QuestionWidget from '../src/components/QuestionWidget';
+
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
+};
 
 export default function QuizPage() {
+  
+
   const router = useRouter();
   const { name } = router.query;
+
+  const [screenState, setScreenState] = useState(screenStates.LOADING);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const question = db.questions[questionIndex];
+
+  const submitAnswer = () => {
+    // logica
+  }
+
+  useEffect(() => {
+    // fetch aqui
+    setTimeout(() => setScreenState(screenStates.QUIZ), 1000);
+  }, []);
 
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-            {/*<Form onSubmit={submit}>
-              <Input
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                placeholder="Seu nome aqui..."
-              />
-              <Button type="submit" disabled={name.trim().length === 0}>
-                JOGAR
-              </Button>
-            </Form>*/}
-          </Widget.Content>
-        </Widget>
+        {
+          screenState === screenStates.QUIZ 
+          && 
+          <QuestionWidget 
+            question={question}
+            questionIndex={questionIndex}
+            totalQuestions={db.questions.length}
+            onSubmit={submitAnswer}
+          />
+        }
+
+        {screenState === screenStates.LOADING && <LoadingWidget />}
         <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/jeffersondenilson/next-quiz" />
