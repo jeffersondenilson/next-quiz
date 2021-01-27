eda89f56237a8260613535857723467761b5dbc4
jeffersondenilson <jefersond121212@gmail.com>
2021-01-27 17:15:35 -0300

    layout base

--- pages/quiz.js
+++ pages/quiz.js
@@ -3,7 +3,12 @@ import { useRouter } from 'next/router';
 import db from '../db.json';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizContainer from '../src/components/QuizContainer';
+import QuizLogo from '../src/components/QuizLogo';
 import Widget from '../src/components/Widget';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Form from '../src/components/Form';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function QuizPage() {
   const router = useRouter();
@@ -12,15 +17,28 @@ export default function QuizPage() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{name ? `Olá, ${name}` : ''}</p>
+            <p>{db.description}</p>
+            {/*<Form onSubmit={submit}>
+              <Input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                placeholder="Seu nome aqui..."
+              />
+              <Button type="submit" disabled={name.trim().length === 0}>
+                JOGAR
+              </Button>
+            </Form>*/}
           </Widget.Content>
         </Widget>
+        <Footer />
       </QuizContainer>
-    </QuizBackground> 
+      <GitHubCorner projectUrl="https://github.com/jeffersondenilson/next-quiz" />
+    </QuizBackground>
   );
 }
