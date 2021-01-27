e51040970a79b530d0ff3fc7720d21f9aa615647
jeffersondenilson <jefersond121212@gmail.com>
2021-01-27 20:07:25 -0300

    loading widget

---
+++ src/components/LoadingWidget/index.js
@@ -0,0 +1,21 @@
+import Widget from '../Widget';
+
+export default function LoadingWidget(){
+  return (
+    <Widget>
+      <Widget.Header>
+        <h3>Carregando...</h3>
+      </Widget.Header>
+      <Widget.Content>
+        <span style={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
+          <img
+            src="https://i.gifer.com/ZZ5H.gif"
+            alt="carregando"
+            width="50px"
+            height="50px"
+          />
+        </span>
+      </Widget.Content>
+    </Widget>
+  );
+}
